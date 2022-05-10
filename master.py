43c1ae0549081882148de45b5d044880d531c40f
Olstertecn11 <otzunund@miumg.edu.gt>
2022-05-10 17:10:08 -0600

    no se queputas

--- master.py
+++ master.py
@@ -2,6 +2,11 @@ import threading
 from tkinter import *
 import tkinter as tk
 import serial
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+
 root = tk.Tk()
 COM = "/dev/ttyUSB0"
 ser = serial.Serial(port=COM, baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=2)
@@ -39,6 +44,20 @@ ReadSerial()
 inputData.grid()
 readData.grid()
 buttonMsg.grid()
+
+
+def createGra():
+    data2 = {'Year': [1920,1930,1940,1950,1960,1970,1980,1990,2000,2010],
+         'Unemployment_Rate': [9.8,12,8,7.2,6.9,7,6.5,6.2,5.5,6.3]
+        }
+    df2 = DataFrame(data2,columns=['Year','Unemployment_Rate'])
+    figure2 = plt.Figure(figsize=(5,4), dpi=100)
+    ax2 = figure2.add_subplot(111)
+    line2 = FigureCanvasTkAgg(figure2, root)
+    line2.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+    df2 = df2[['Year','Unemployment_Rate']].groupby('Year').sum()
+    df2.plot(kind='line', legend=True, ax=ax2, color='r',marker='o', fontsize=10)
+    ax2.set_title('Year Vs. Unemployment Rate') 
 
 
 # GUI thread

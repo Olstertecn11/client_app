9a81ca0d5ebda9d752890a9f17d92b79d48df03c
Olivers11 <olstertecn597@outlook.com>
2022-05-27 01:33:22 -0600

    _bluetooth file regognize device and read data from arduino and bluetooth module

--- _bluetooth.py
+++ _bluetooth.py
@@ -1,41 +1,15 @@
-# import bluetooth
+import socket
 
-# nearby_devices = bluetooth.discover_devices(lookup_names=True)
-# print("found %d devices" % len(nearby_devices))
-
-# for addr, name in nearby_devices:
-#     print("  %s - %s" % (addr, name))
-
-import sys
-import bluetooth
+_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+_socket.connect(('00:21:07:00:55:5F', 1))
+_socket.send(b"0")
 
 
-sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
-#l2capclient.py 524 2007-08-15 04:04:52Z albert 
-
-if len(sys.argv) < 2:
-    print("Usage: l2capclient.py <addr>")
-    sys.exit(2)
-
-bt_addr = sys.argv[1]
-port = 0x1001
-
-print("Trying to connect to {} on PSM 0x{}...".format(bt_addr, port))
-
-sock.connect((bt_addr, port))
-
-print("Connected. Type something...")
 while True:
-    data = input()
-    if not data:
-        break
-    sock.send(data)
-    data = sock.recv(1024)
-    print("Data received:", str(data))
-
-sock.close()
+    print(_socket.recv(1024))
 
 
+_socket.close()
 
 
 

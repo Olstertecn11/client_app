917df35ecb9f889a118fec86e33a1f225d58b373
Olivers11 <olstertecn597@outlook.com>
2022-05-31 07:55:29 -0600

    pass master to objective oriented programmming

--- master.py
+++ master.py
@@ -2,42 +2,41 @@ from tkinter import *
 import time
 from threading import *
   
-# Create Object
 root = Tk()
-  
-# Set geometry
 root.geometry("400x400")
   
-# use threading
   
-continue_process = False 
 
-def stop():
-    # Call work function
-    global continue_process
-    continue_process = False
-  
-def start_process():
-    global continue_process 
-    continue_process = True
-    t1 = Thread(target=work)
-    t1.start()
 
-# work function
-def work():
-    global continue_process
-    print("sleep time start")
-    i = 0
-    while continue_process:
-        print(i)
-        time.sleep(1)
-        i +=1
+class _Interface(object):
+
+    def __init__(self, master):
+        self.frame = master
+        self.continue_process = False
+        self.createButtons()
+
+    def stop(self):
+        # Call work function
+        self.continue_process = False
   
-    print("sleep time stop")
+    def start_process(self):
+        self.continue_process = True
+        t1 = Thread(target=self.work)
+        t1.start()
+
+    def work(self):
+        print("sleep time start")
+        i = 0
+        while self.continue_process:
+            print(i)
+            time.sleep(1)
+            i +=1
+      
+        print("sleep time stop")
+      
+    def createButtons(self):
+        Button(self.frame,text="Start",command = self.start_process).pack()
+        Button(self.frame, text="Stop", command = self.stop).pack()
   
-# Create Button
-Button(root,text="Start",command = start_process).pack()
-Button(root, text="Stop", command = stop).pack()
-  
-# Execute Tkinter
+application = _Interface(root)
 root.mainloop()

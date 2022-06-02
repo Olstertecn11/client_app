01f4773f8e5dc483b9019ed62dc86b8fbbd67478
Frlso3 <lafm2002@gmail.com>
2022-06-01 21:52:11 -0600

    Se modifica modulo de graficar desde excel

--- grafico_excel.py
+++ grafico_excel.py
@@ -1,12 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-workbook1 = pd.ExcelFile(r'C:\Users\Luis\Desktop\pacientes.xlsx')
+workbook1 = pd.ExcelFile(r'C:\Users\Luis\Desktop\5to semestre\Progra III\client_app\pacientes.xlsx')
 
 df= pd.read_excel(workbook1)
 
 valores= df[['Nombre', 'Edad', 'Pulso', 'Altura', 'Peso']]
-
-ax= valores.plot.line( x='Nombre', y=['Edad','Pulso', 'Altura', 'Peso'])
+x= valores['Nombre']
+y1= valores['Edad']
+y2= valores['Pulso']
+y3= valores['Altura']
+y4= valores['Peso']
+figure, axis = plt.subplots(2, 2)
+plt.xlabel('Nombre')
+plt.xlabel('Nombre')
+plt.xlabel('Nombre')
+plt.xlabel('Nombre')
+figure.suptitle('Datos de pacientes')
+axis[0, 0].plot(x, y1)
+axis[0, 0].set_title("Nombre x Edad")
+axis[0,1].plot(x, y2)
+axis[0,1].set_title("Nombre x Pulso")
+axis[1,0].plot(x, y3)
+axis[1,0].set_title("Nombre x Altura")
+axis[1,1].plot(x, y4)
+axis[1,1].set_title("Nombre x Peso")
 plt.show()
-
